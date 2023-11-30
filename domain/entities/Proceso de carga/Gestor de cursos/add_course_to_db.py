849bc73697c2e29728328ed738b8e6315bc21576
anamarigarzon <anamari.garzon@urosario.edu.co>
2023-11-30 14:04:53 -0500

    Update add_course_to_db.py

--- domain/entities/Proceso de carga/Gestor de cursos/add_course_to_db.py
+++ domain/entities/Proceso de carga/Gestor de cursos/add_course_to_db.py
@@ -4,5 +4,5 @@ import create_course_dictionary
 def add_course_to_db(course):
     course_dictionary = create_course_dictionary(course)
     json_object = json.dumps(course_dictionary, indent=4)
-    with open("sample.json", "w") as outfile:
+    with open("../../../database/database.json", "w") as outfile:
         outfile.write(json_object)
